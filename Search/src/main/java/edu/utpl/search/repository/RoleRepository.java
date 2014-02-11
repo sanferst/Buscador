f59619686e24f64464fec6a42ee8440d40bf7443
Santiago Aguilera <sgaguilera@utpl.edu.ec>
2014-02-11 18:19:47 -0500

    Ignoracion de archivos

---
+++ Search/src/main/java/edu/utpl/search/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package edu.utpl.search.repository;
+
+import edu.utpl.search.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
