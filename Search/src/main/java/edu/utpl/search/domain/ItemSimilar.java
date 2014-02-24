95a67997c6499931dc89f7ca3849cd4aa0d568b3
Santiago Aguilera <sgaguilera@utpl.edu.ec>
2014-02-24 12:21:37 -0500

    cambios para correccion de mapeo

--- Search/src/main/java/edu/utpl/search/domain/ItemSimilar.java
+++ Search/src/main/java/edu/utpl/search/domain/ItemSimilar.java
@@ -64,6 +64,126 @@ public class ItemSimilar implements Serializable {
     @JsonProperty("score")
     private String score;
 
+    public String getStore() {
+        return store;
+    }
+
+    public void setStore(String store) {
+        this.store = store;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUniversidad() {
+        return universidad;
+    }
+
+    public void setUniversidad(String universidad) {
+        this.universidad = universidad;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getContinet() {
+        return continet;
+    }
+
+    public void setContinet(String continet) {
+        this.continet = continet;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getResourseType() {
+        return resourseType;
+    }
+
+    public void setResourseType(String resourseType) {
+        this.resourseType = resourseType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getTextConcat() {
+        return textConcat;
+    }
+
+    public void setTextConcat(String textConcat) {
+        this.textConcat = textConcat;
+    }
+
     public String getScore() {
         return score;
     }
@@ -71,124 +191,6 @@ public class ItemSimilar implements Serializable {
     public void setScore(String score) {
         this.score = score;
     }
-    
-    public String getStore() {
-        return store;
-    }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getUniversidad() {
-        return universidad;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getLicense() {
-        return license;
-    }
-
-    public String getContinet() {
-        return continet;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public String getResourseType() {
-        return resourseType;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getLanguaje() {
-        return language;
-    }
-
-    public String getTextConcat() {
-        return textConcat;
-    }
-
-    public void setStore(String store) {
-        this.store = store;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setUniversidad(String universidad) {
-        this.universidad = universidad;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setLicense(String license) {
-        this.license = license;
-    }
-
-    public void setContinet(String continet) {
-        this.continet = continet;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public void setResourseType(String resourseType) {
-        this.resourseType = resourseType;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public void setTextConcat(String textConcat) {
-        this.textConcat = textConcat;
-    }
+   
 }
--- Search/src/main/java/edu/utpl/search/domain/ResultadoSimilar.java
+++ Search/src/main/java/edu/utpl/search/domain/ResultadoSimilar.java
@@ -15,7 +15,7 @@ import org.codehaus.jackson.annotate.JsonProperty;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ResultadoSimilar implements Serializable {
-
+    
     @JsonProperty("q")
     private String query;
     
@@ -34,51 +34,55 @@ public class ResultadoSimilar implements Serializable {
     @JsonProperty("results")
     private List<ItemSimilar> itemSimilar;
 
+    
     public String getQuery() {
         return query;
-    }
-
-    public String getFecha() {
-        return fecha;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public int getRows() {
-        return rows;
-    }
-
-    public double getTiempo() {
-        return tiempo;
-    }
-
-    public List<ItemSimilar> getItemSimilar() {
-        return itemSimilar;
     }
 
     public void setQuery(String query) {
         this.query = query;
     }
 
+    public String getFecha() {
+        return fecha;
+    }
+
     public void setFecha(String fecha) {
         this.fecha = fecha;
+    }
+
+    public String getVersion() {
+        return version;
     }
 
     public void setVersion(String version) {
         this.version = version;
     }
 
+    public int getRows() {
+        return rows;
+    }
+
     public void setRows(int rows) {
         this.rows = rows;
+    }
+
+    public double getTiempo() {
+        return tiempo;
     }
 
     public void setTiempo(double tiempo) {
         this.tiempo = tiempo;
     }
 
+    public List<ItemSimilar> getItemSimilar() {
+        return itemSimilar;
+    }
+
     public void setItemSimilar(List<ItemSimilar> itemSimilar) {
         this.itemSimilar = itemSimilar;
     }
+
+    
+   
 }
